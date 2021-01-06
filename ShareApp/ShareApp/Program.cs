5ffc6a12d52d2b76d5edf6803b6fcac5235d6a25
Ian Broderick <ianbroderick580@gmail.com>
2021-01-06 15:30:03 +0100

    Create application

--- ShareApp/ShareApp/Program.cs
+++ ShareApp/ShareApp/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Ian.ShareApp
+﻿using System.Collections.Generic;
+
+namespace Ian.ShareApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-
+            _ = new ShareApplication(new List<User> { new User("John"), new User("Peter"), new User("Mary") });
         }
     }
 }
