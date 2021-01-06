9729ef783c20369310d9c13d3cf01f758f271940
Ian Broderick <ianbroderick580@gmail.com>
2021-01-06 15:30:17 +0100

    Ctor and integration test

--- ShareApp/ShareAppTest/ShareAppTest.cs
+++ ShareApp/ShareAppTest/ShareAppTest.cs
@@ -1,6 +1,6 @@
 using FluentAssertions;
 using Ian.ShareApp;
-using Moq;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -12,6 +12,37 @@ namespace Ian.ShareAppTest
         public void ApplicationExists()
         {
             _ = new ShareApplication(new List<User>());
+        }
+
+        [Fact]
+        public void UsersMustNotBeNull()
+        {
+            Action constructApp = () => new ShareApplication(null);
+
+            constructApp.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void IntegrationTest()
+        {
+            // Arrange
+            var john = new User("John");
+            var peter = new User("Peter");
+            var mary = new User("Mary");
+
+            var shareApp = new ShareApplication(new List<User> { john, peter, mary });
+
+            // Act
+            shareApp.AddExpense(new Expense("Hotel", 500, john));
+            shareApp.AddExpense(new Expense("Food", 150, mary));
+            shareApp.AddExpense(new Expense("Sightseeing", 100, peter));
+
+            // Assert
+            shareApp.GetExpenseTotal().Should().Be(750);
+            
+            shareApp.GetUsersBalance(john).Should().Be(250);
+            shareApp.GetUsersBalance(mary).Should().Be(-100);
+            shareApp.GetUsersBalance(peter).Should().Be(-150);
         }
     }
 }
